d63df07c5e0e6d65a5d2f8dc3002540ffe30aa12
Art Agu <kmpagk003@gmail.com>
2024-04-12 09:59:39 +0530

    error_boundary_commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [value, setValue] = useState(0);
+  try {
+    if (value > 3) {
+      throw new Error("Danger Zone");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  
+  return (
+    <div className="App">
+      <h3>{value}</h3>
+      <button onClick={() => setValue(value + 1)}> Increment</button>
+    </div>
+  );
+}
+
+export default App;
